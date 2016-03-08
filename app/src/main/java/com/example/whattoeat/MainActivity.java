0ab13f39bbe67129598f44a2e0c9724dd593aca1
Promlert Lovichit <promlert@gmail.com>
2016-03-09 00:25:56 +0700

    Add food picture dialog

--- app/src/main/java/com/example/whattoeat/MainActivity.java
+++ app/src/main/java/com/example/whattoeat/MainActivity.java
@@ -1,19 +1,29 @@
 package com.example.whattoeat;
 
 import android.app.AlertDialog;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     String[] foodArray = new String[]{
-        "ข้าวผัด", "ข้าวมันไก่", "ข้าวหน้าเป็ด",
-        "ราดหน้า", "ผัดซีอิ๊ว", "ผัดไทย"
+            "ข้าวผัด", "ข้าวไข่เจียว", "ข้าวหน้าเป็ด",
+            "ข้าวมันไก่", "ข้าวหมูแดง", "ราดหน้า",
+            "ผัดซีอิ๊ว", "ผัดไทย", "ส้มตำไก่ย่าง"
+    };
+
+    int[] foodPictureArray = new int[]{
+            R.drawable.kao_pad, R.drawable.kao_kai_jeaw, R.drawable.kao_na_ped,
+            R.drawable.kao_mun_kai, R.drawable.kao_moo_dang, R.drawable.rad_na,
+            R.drawable.pad_sie_eew, R.drawable.pad_thai, R.drawable.som_tum_kai_yang
     };
 
     @Override
@@ -28,18 +38,25 @@ public class MainActivity extends AppCompatActivity {
                 Random r = new Random();
                 int randomIndex = r.nextInt(foodArray.length);
                 String randomFood = foodArray[randomIndex];
+                int randomFoodPicture = foodPictureArray[randomIndex];
 
                 AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
-                dialog.setTitle("What To Eat");
-                dialog.setMessage("You may want to eat: " + randomFood);
-                dialog.show();
 
-                Toast t = Toast.makeText(
-                        getApplicationContext(),
-                        randomFood,
-                        Toast.LENGTH_SHORT
-                );
-                t.show();
+                // เข้าถึงตัว Inflater ของระบบ Android
+                LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                // ทำการ inflate layout ให้เป็น object
+                View layout = inflater.inflate(R.layout.dialog_food, null);
+
+                // อ้างอิงไปยัง ImageView (แสดงภาพอาหาร) ใน layout
+                ImageView dishImageView = (ImageView) layout.findViewById(R.id.dish_image_view);
+                dishImageView.setImageResource(randomFoodPicture);
+
+                // อ้างอิงไปยัง TextView (แสดงชื่ออาหาร) ใน layout
+                TextView dishNameTextView = (TextView) layout.findViewById(R.id.dish_name_text_view);
+                dishNameTextView.setText(randomFood);
+
+                dialog.setView(layout);
+                dialog.show();
             }
         });
     }
